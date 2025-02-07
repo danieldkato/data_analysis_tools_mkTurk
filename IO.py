e4f277d96da1c3f8220c8f597f1a8707db5a9098
Daniel D Kato <daniel.d.kato@gmail.com>
2025-02-07 10:37:38 -0500

    Add code for getting THREEJS parameters in ch_dicts_2_h5()

--- IO.py
+++ IO.py
@@ -8,6 +8,7 @@ import warnings
 import re
 import numpy as np
 import pandas as pd
+import json
 from itertools import product
 from data_analysis_tools_mkTurk.utils_meta import find_channels, get_recording_path, get_coords_sess, get_all_metadata_sess
 from data_analysis_tools_mkTurk.stim_info import filter_stim_trials, expand_classes, get_class_trials, create_trial_df, create_stim_idx_mat, reverse_lookup_rsvp_stim, session_dicts_2_df, sess_meta_dict_2_df
@@ -122,20 +123,6 @@ def ch_dicts_2_h5(base_data_path, monkey, date, preprocessed_data_path, channels
     trial_params_df['idx_merge'] = np.arange(n_rows)
     trial_params_df = trial_params_df.set_index('idx_merge')
     
-    # Add a few general parameters to trial_params_df:
-    # TODO: think about adding following parameters as well:
-    # From stim_meta (one value per dict): iti_dur, t_before, t_after 
-    # From sess_meta: reward, reward_dur
-    monkey_col = [monkey] * n_rows 
-    date_col = [date] * n_rows
-    reward_bool = sess_meta_df['reward_bool']
-    extra_params_df = pd.DataFrame({'monkey' : monkey_col, 
-                                    'date' : date_col, 
-                                    'reward_bool' : reward_bool})
-    extra_params_df['idx'] = np.arange(n_rows) # < Only need this temporarily to concatenate dataframes
-    extra_params_df = extra_params_df.set_index('idx')
-    trial_params_df = pd.concat([extra_params_df, trial_params_df], axis=1)
-    
     # Apply offsets to stim_idx; recall if scenefile b follows scenefile a with
     # m images, then index of first image of scenefile b will be m, not 0:
     offsets_df = trial_params_df[['scenefile', 'stim_idx']].groupby('scenefile').min().reset_index()
@@ -143,6 +130,50 @@ def ch_dicts_2_h5(base_data_path, monkey, date, preprocessed_data_path, channels
     trial_params_df = pd.merge(trial_params_df, offsets_df, on=['scenefile'])
     trial_params_df['stim_idx'] = trial_params_df['stim_idx'] - trial_params_df['offset']
     trial_params_df['stim_idx'] = trial_params_df['stim_idx'].drop(columns='offset')
+    
+    # Try to retrieve THREEJS params directly from behavior files:
+    behav_df = pd.DataFrame()
+    sess_dirs = [x for x in os.listdir(os.path.join(base_data_path, monkey)) if date in x]
+    if len(sess_dirs) == 1 and os.path.exists(os.path.join(base_data_path, monkey, sess_dirs[0])):
+        sess_dir = os.path.join(base_data_path, monkey, sess_dirs[0])
+        behav_files = np.unique(trial_params_df.behav_file)
+
+        # Iterate over behavior files:
+        for b in behav_files:
+            bpath = os.path.join(sess_dir, b+'.json')
+            bfile = json.load(open(bpath, 'rb'))
+            curr_scenefiles = bfile['TASK']['ImageBagsSample']
+            
+            # Iterate over scenefiles:
+            for s, sfile in enumerate(curr_scenefiles):
+                curr_sfile_df = pd.DataFrame()
+                n_stim = bfile['SCENES']['SampleScenes'][s]['nimages']
+                curr_sfile_df['stim_idx'] = np.arange(n_stim)
+                dims = ['x', 'y', 'z']
+                for dim in dims:
+                    dat =  bfile['SCENES']['SampleScenes'][s]['CAMERAS']['camera00']['targetTHREEJS'][dim]
+                    if len(dat) == n_stim:
+                        curr_sfile_df['targetTHREEJS_'+dim] = dat
+                    elif len(dat) == 1:
+                        curr_sfile_df['targetTHREEJS_'+dim] = dat[0]*np.ones(n_stim)
+                    else:
+                        curr_sfile_df['targetTHREEJS_'+dim] = [None]*n_stim
+                curr_sfile_df['scenefile'] = sfile
+                curr_sfile_df['behav_file'] = b
+                behav_df = pd.concat([behav_df, curr_sfile_df], axis=0)
+    trial_params_df['dummy'] = 1
+    trial_params_df = pd.merge(trial_params_df, behav_df, on=['scenefile', 'behav_file', 'stim_idx'], how='outer')
+    trial_params_df = trial_params_df[~trial_params_df.dummy.isna()]
+    trial_params_df = trial_params_df.drop(columns=['dummy'])     
+    trial_params_df['trial_num'] = trial_params_df.trial_num.astype(int)       
+    
+    # Add a few general parameters to trial_params_df:
+    # TODO: think about adding following parameters as well:
+    # From stim_meta (one value per dict): iti_dur, t_before, t_after 
+    # From sess_meta: reward, reward_dur
+    trial_params_df['monkey'] = monkey    
+    trial_params_df['date'] = date
+    trial_params_df['reward_bool'] = sess_meta_df['reward_bool']
     
     # Try to get paths to saved images:
     trial_params_df = find_im_full_paths(trial_params_df, base_data_path)
@@ -776,6 +807,10 @@ def find_im_full_paths(trial_params_df, local_data_path=None):
         
         # Get full paths to saved images, add to dataframe:
         impaths = curr_unique_images_df.apply(lambda x : stim_idx_2_img_path(x.sfile_imdir, x.stim_idx), axis=1)
+        print('monkey={}'.format(monkey))
+        print('date={}'.format(date))
+        print('type(impaths)={}'.format(type(impaths)))
+        print('curr_unique_images_df.shape={}'.format(curr_unique_images_df.shape))
         curr_unique_images_df['img_full_path'] = impaths
         curr_unique_images_df['date'] = date
         unique_images_df = pd.concat([unique_images_df, curr_unique_images_df], axis=0)
