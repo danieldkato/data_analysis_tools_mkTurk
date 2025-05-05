13e53a806dafe644f3ff81bde6e96b0550c82ae2
Daniel D Kato <daniel.d.kato@gmail.com>
2025-05-05 16:36:28 -0400

    Fix typo in default tip_length

--- npix.py
+++ npix.py
@@ -132,7 +132,7 @@ def generate_imro_table(length='short', parity='columnar', short_bank=0, n=384, 
 def h5_2_site_coords_df(h5path):
     zero_coords = pd.read_hdf(h5path, 'zero_coordinates')
     imro_tbl = pd.read_hdf(h5path, 'imro_table')
-    site_coords_df = get_site_coords(zero_coords, imro_tbl, spacing=15, tip_length=1725)
+    site_coords_df = get_site_coords(zero_coords, imro_tbl, spacing=15, tip_length=175)
     return site_coords_df
 
 
